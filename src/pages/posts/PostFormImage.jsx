102e6b7041ba02ace18f4a7dcb4bb2def02e0f82
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-08 13:29:23 +0300

    Form is ready

--- src/pages/posts/PostFormImage.jsx
+++ src/pages/posts/PostFormImage.jsx
@@ -41,20 +41,45 @@ return(
 
         <form onSubmit={handleSubmit}>
 
+          Media Type:
+
+        <select name="mediaType" value={formData.mediaType} onChange={handleChange}>
+
+              <option value="image">Image</option>
+              <option value="video">Video</option>     
+        </select>   
+
+        Media URL:
+
+        <input type="url" name="mediaUrl" value={formData.mediaUrl} onChange={handleChange} placeholder="https://..."   />
+
+        Caption:
+
+        <textarea name="caption" value={formData.caption}onChange={handleChange} />
+
+        Category:
+
+        <select name="category" value={formData.category} onChange={handleChange} >
             
+             <option value="">Select a category</option>
+             <option value="fashion">Fashion</option>
+             <option value="food">Food</option>
+             <option value="travel">Travel</option>
+             <option value="technology">Technology</option>
+             <option value="education">Education</option>
+             <option value="comedy">Comedy</option>
+             <option value="lifestyle">Lifestyle</option>
+             <option value="other">Other</option>
+        </select>                    
+
+        <button type="submit"> Publish </button>
 
         </form>
-
-
-
 
     </main>
 
 
-
-
 )
-
 
 
 
