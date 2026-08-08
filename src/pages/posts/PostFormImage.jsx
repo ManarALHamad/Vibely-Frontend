a29260852c0aa07cfd777feb8704444c0980a04f
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-08 13:13:02 +0300

    working on the form

--- src/pages/posts/PostFormImage.jsx
+++ src/pages/posts/PostFormImage.jsx
@@ -2,12 +2,61 @@
 import { useState } from "react"
 import { useNavigate } from "react-router"
 
-const PostFormImage = () => {
+const PostFormImage = (props) => {
+
+const navigate = useNavigate()
+
+const initialState ={
+
+    mediaType: "image",
+    mediaUrl: "",
+    caption: "",
+    category: "",
+
+}
+
+const [formData, setFormData] = useState(initialState)
+
+
+ const handleChange = (event) => {
+  setFormData({...formData,[event.target.name]: event.target.value})
+
+    }
+const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        await props.addPost(formData)
+
+        setFormData(initialState)
+
+        navigate("/posts")
+    }
+
 
 return(
 
-    <h1>form</h1>
+    <main>
+
+        <h2>Create a post</h2>
+
+        <form onSubmit={handleSubmit}>
+
+            
+
+        </form>
+
+
+
+
+    </main>
+
+
+
+
 )
+
+
+
 
 }
 
