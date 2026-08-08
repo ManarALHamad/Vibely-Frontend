aebfa79d3e09e5e1792c4ca2b25263dc2edb1793
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-08 13:36:50 +0300

    comment form is created

---
+++ src/pages/comments/CommentForm.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react"
+
+const CommentForm = (props) => {
+
+    const initialState = {
+        content: ""
+    }
+
+    const [formData, setFormData] = useState(initialState)
+
+    const handleChange = (event) => {
+
+        setFormData({...formData,[event.target.name]: event.target.value })
+        
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+
+        await props.addComment(formData)
+
+        setFormData(initialState)
+    }
+
+    return (
+
+        <form onSubmit={handleSubmit}>
+
+            <textarea name="content" value={formData.content} onChange={handleChange} placeholder="Write a comment..." />
+        
+
+            <button type="submit"> Post Comment</button>
+               
+            
+
+        </form>
+
+    )
+}
+
+export default CommentForm
