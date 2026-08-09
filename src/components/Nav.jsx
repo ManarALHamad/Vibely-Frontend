d8da90ae54b8f8f2d594bf64011507b1d3426ce5
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-09 23:30:39 +0300

    nav bar is updated for both signedIn and not signedIn users

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 
-
 const Nav = (props) => {
 
     const handleSignOut = () => {
@@ -10,30 +9,42 @@ const Nav = (props) => {
 
     return (
         <nav>
-            <Link className="nav-brand" to="/">App Name</Link>
-            { props.user ? (
-                <ul>
-                    <li>
-                        <Link to="/">Dashboard</Link>
-                    </li>
-                    <li>Welcome, {props.user.username}!</li>
-                    <li>
-                        <Link to="/" onClick={handleSignOut}>Sign Out</Link>
-                    </li>
-                </ul>
-            ) : (
-            <ul>
-                <li>
-                    <Link to='/'>Home</Link>
-                </li>
-                <li>
-                    <Link to='/sign-up'>Sign Up</Link>
-                </li>
-                <li>
-                    <Link to='/sign-in'>Sign In</Link>
-                </li>
-            </ul>
-            ) }
+
+            <Link className="nav-brand" to="/">
+                Vibely
+            </Link>
+
+            {props.user ? (
+
+          <ul>
+
+          <li>Welcome, {props.user.username}!</li>
+
+           <li> <Link to="/">FEED  </Link> </li>     
+                       
+           <li>  <Link to="/posts/new"> NEW POST </Link> </li>            
+                       
+           <li>  <Link to="/profile"> PROFILE </Link> </li>
+                    
+           <li>  <Link to="/" onClick={handleSignOut}> Sign Out </Link>  </li>      
+   
+            </ul> ) : (
+               //not a user  
+             <ul>
+
+            <li> <Link to="/"> Home</Link> </li>
+
+            <li>  <Link to="/sign-up"> Sign Up  </Link> </li>   
+                        
+             <li> <Link to="/sign-in"> Sign In </Link> </li>
+                        
+              </ul>               
+                        
+                    
+
+               
+
+            )}
 
         </nav>
     )
