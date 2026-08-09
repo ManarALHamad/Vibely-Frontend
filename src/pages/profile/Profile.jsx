fc11e10f7601e21ce1192c7916e63e210c90fe0c
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-10 00:02:30 +0300

    profile page is created and being tested succesfully

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,2 +1,15 @@
 // here the user can view their username, profile photo, posts(images and videoes)
 
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+const Profile = (props) => {
+
+return (
+    <>
+    <h1>profile</h1>
+    </>
+)
+}
+
+export default Profile
