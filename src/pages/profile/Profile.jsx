15848d3bf473264e829d5cf863ed84ba86b64064
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-10 00:18:54 +0300

    profile page is working

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -5,11 +5,83 @@ import { Link } from "react-router-dom"
 
 const Profile = (props) => {
 
+const [myPosts, setMyPosts] = useState([])
+
+useEffect(() => {
+
+    const userPosts = props.posts.filter((post) => {
+
+        const authorId =
+            typeof post.author === "object"
+                ? post.author?._id
+                : post.author
+
+        return authorId === props.user?._id
+    })
+
+    setMyPosts(userPosts)
+
+}, [props.posts, props.user])
+
+
+
 return (
-    <>
-    <h1>profile</h1>
-    </>
+  
+  <main>
+
+<h1>My Profile</h1>
+
+<img src={props.user.profileImage} alt={props.user.username} width="150" />
+                
+<h2>{props.user.username}</h2>                
+                
+<h3>My Posts</h3>
+
+{myPosts.length === 0 ? (
+
+  <p>No posts yet.</p> ) : (
+
+ myPosts.map((post) => (           
+               
+<div key={post._id}>
+                    
+ {post.mediaType === "image" ? (
+                       
+  <img src={post.mediaUrl} alt={post.caption} width="300"  />
+
+  ) : (
+ <video src={post.mediaUrl} controls width="300"/>
+                           
+   )}                               
+                                
+  <h3>{post.caption}</h3>           
+
+  <p>{post.category}</p>                  
+
+  <Link to={`/posts/${post._id}`}>  View  </Link>   {" | "}               
+
+  <Link to={`/posts/${post._id}/edit`}>  Edit </Link>
+                                            
+      </div>                      
+                        
+
+     
+                ))
+
+            )}
+                  
+
+ 
+  </main>
+
+
+
+
+
 )
+
+
+
 }
 
 export default Profile
