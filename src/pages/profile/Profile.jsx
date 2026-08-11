f71ae6a6dcf0ae6c3fc09113f91317f0351bcb84
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-11 14:55:46 +0300

    editing profile page make it more real

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -24,61 +24,123 @@ useEffect(() => {
 }, [props.posts, props.user])
 
 
-
 return (
-  
-  <main>
 
-<h1>My Profile</h1>
+<main className="profile-page">
 
-<img src={props.user.profileImage} alt={props.user.username} width="150" />
-                
-<h2>{props.user.username}</h2>                
-                
-<h3>My Posts</h3>
+ {/* PROFILE HEADER */}
+ <section className="profile-header">
 
-{myPosts.length === 0 ? (
+  <div className="profile-picture">
 
-  <p>No posts yet.</p> ) : (
-
- myPosts.map((post) => (           
-               
-<div key={post._id}>
-                    
- {post.mediaType === "image" ? (
-                       
-  <img src={post.mediaUrl} alt={post.caption} width="300"  />
-
-  ) : (
- <video src={post.mediaUrl} controls width="300"/>
-                           
-   )}                               
-                                
-  <h3>{post.caption}</h3>           
-
-  <p>{post.category}</p>                  
-
-  <Link to={`/posts/${post._id}`}>  View  </Link>   {" | "}               
-
-  <Link to={`/posts/${post._id}/edit`}>  Edit </Link>
-                                            
-      </div>                      
-                        
-
-     
-                ))
-
-            )}
-                  
-
+   <img src={props.user.profileImage || "https://i.pravatar.cc/150" }alt={props.user.username} />
+                            
  
-  </main>
+ </div>
+
+   <div className="profile-info">
+
+    <h1>{props.user.username}</h1>
+
+      <div className="profile-stats">
+
+      <div>
+       {myPosts.length}
+      <span>Posts</span>
+        </div>
+
+         <div>
+          0
+        <span>Followers</span>
+
+          </div>
+
+         <div>
+          0
+        <span>Following</span>
+        </div>
+
+        </div>
+
+         <div className="profile-actions">
+
+         <Link to="/posts/new">
+           <button>Create Post</button>
+         </Link>
+         
+         {/* edit profile link or page */}
+
+         <button>Edit Profile</button>
+
+         </div>
+
+         </div>
+
+            </section>
 
 
+    {/* POSTS SECTION */}
+
+    <section className="profile-posts">
+
+     <h2>My Posts</h2>
+
+    {myPosts.length === 0 ? (
+
+     <div className="no-posts">
+
+      <h3>No posts yet</h3>
+
+      <p>Share your first post on Vibely!</p>
+
+        <Link to="/posts/new"> Create Post</Link>
+       
+     </div>
+
+     ) : (
+
+      <div className="profile-post-grid">
+
+      {myPosts.map((post) => (
+
+        <Link to={`/posts/${post._id}`} key={post._id} className="profile-post">
+    
+         {post.mediaType === "image" ? (
+
+         <img src={post.mediaUrl} alt={post.caption} width={300} />
+                                        
+        ) : (
+
+            // video not yet
+
+         <video src={post.mediaUrl} />
+                                        
+                                   
+     )}
+
+        <div className="profile-post-info">
+        <p>{post.caption}</p>
+       <span> {post.category}</span>
+                                    
+        </div>
+
+                                    
+
+                              
+              </Link>              
+
+                        ))}
+
+                    </div>
+
+                )}
+
+            </section>
+
+        </main>
+    )
 
 
-
-)
 
 
 
