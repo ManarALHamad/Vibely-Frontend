3f216651107ce36608757523a3ff980b2f6e0a66
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-12 14:59:15 +0300

    now we can follow and un follw and view following and follwers but still the profile page does not show that

--- src/pages/profile/AllProfiles.jsx
+++ src/pages/profile/AllProfiles.jsx
@@ -31,46 +31,95 @@ useEffect(() => {
 
 }, [])
 
+const handleFollow = async (userId) => {
+
+        try {
+
+            const updatedUser = await userService.toggleFollow(userId)
+
+            const updatedUsers = allUsers.map((user) => {
+
+                if (user._id === userId) {
+                    return updatedUser
+                }
+
+                return user
+            })
+
+            setAllUsers(updatedUsers)
+
+        } catch (error) {
+
+            console.log(error)
+        }
+    }
 
 return (
 
-    <main className="profiles-page">
+<main className="profiles-page">
 
-        <h1>All Profiles</h1>
+<h1>All Profiles</h1>
 
-    <div className="profiles-container">
+<div className="profiles-container">
 
-    {allUsers.map((user) => (    
-
-        <div className="profile-card" key={user._id}>
-
-         
-
-         <h2>{user.username}</h2>
+{allUsers.map((user) => {
 
 
-        <div className="profiles-stats">
 
-         <span> {user.followers?.length || 0} Followers </span>
+const isFollowing = user.followers?.some((follower) => {
+
+     const followerId =
+     typeof follower === "object"
+     ? follower._id
+     : follower
+
+    return followerId === props.user?._id
+     })
+
+
+   return (
+
+     <div className="profile-card"key={user._id} >
+                            
+                            
+    <h2>{user.username}</h2>
+
+
+    <div className="profiles-stats">
+
+    <span> {user.followers?.length || 0} Followers </span>
+                                   
+                            
+    <span>{user.following?.length || 0} Following </span>
+                                    
                                
-         <span>  {user.following?.length || 0} Following   </span>               
 
-        </div> 
-           <Link to={`/profiles/${user._id}`}>
-    View Profile
-</Link>
-                             
-  </div>
-       ))}
-
-       </div>
-  
+     </div>
 
 
-    </main>
-)
+     {/* <Link to={`/profiles/${user._id}`}>  View Profile</Link> */}
+                              
+                            
+    {/* don't follow yourself */}
 
+     {user._id !== props.user?._id && (
 
+      <button   onClick={() => handleFollow(user._id)}> {isFollowing  ? "Unfollow"  : "Follow"} </button>                      
+                                   
+ 
+
+     )}
+
+     </div>
+
+        )
+
+        })}
+
+            </div>
+
+        </main>
+    )
 }
 
 
