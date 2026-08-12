5acf343a87d6d1e3cfd172f74e02d58a7c3c8162
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-12 09:18:38 +0300

    starting to work with

--- src/pages/posts/PostList.jsx
+++ src/pages/posts/PostList.jsx
@@ -69,9 +69,9 @@ const PostList = (props) => {
                            
                        
 
-                    </div>
+         </div>
 
-                )
+          )
 
             })}
 
