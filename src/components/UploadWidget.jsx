404132326c6f3691e032834526ab7c6d7bfdd2b1
ManarALHamad <manaralhamad2004@gmail.com>
2026-08-12 09:25:13 +0300

    uploadWidget component

---
+++ src/components/UploadWidget.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useRef } from 'react'
+
+const UploadWidget = () => {
+
+
+    const cloudinaryRef = useRef()
+
+      useEffect(() => {
+      cloudinaryRef.current = window.cloudinary;
+
+  }, [])
+}
+
+
+
+
+
+export default UploadWidget 
